360b7d7bc1fc8b1aec377c939a9d0234420cc0c3
Aleks Kapich <aleks.kapich@gmail.com>
2025-09-06 00:14:10 +0200

    Add RNN classifier head

--- models.py
+++ models.py
@@ -36,3 +36,36 @@ class MLPClassifier(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+
+class RNNClassifier(nn.Module):
+    def __init__(
+        self,
+        input_size=768,
+        hidden_size=128,
+        num_layers=1,
+        output_size=12,
+        bidirectional=False,
+    ):
+        super().__init__()
+        self.bidirectional = bidirectional
+        self.rnn = nn.RNN(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            bidirectional=bidirectional,
+            nonlinearity="tanh",
+        )
+        factor = 2 if bidirectional else 1
+        self.classifier = nn.Linear(hidden_size * factor, output_size)
+
+    def forward(self, x):
+        _, hidden = self.rnn(x)
+
+        if self.bidirectional:
+            last_hidden = torch.cat([hidden[-2], hidden[-1]], dim=1)
+        else:
+            last_hidden = hidden[-1]
+
+        logits = self.classifier(last_hidden)
+        return logits
